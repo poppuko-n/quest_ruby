a9076f1ef665ca04e1d9855ab7bf0be579531c00
poppuko-n <souta0609@icloud.com>
2024-10-14 08:19:24 +0900

    Add war.rb file

---
+++ war.rb
@@ -0,0 +1,24 @@
+# ５２枚のカードの作成
+class Crad
+end
+
+# デッキの作成
+class Deck
+end
+
+
+# ゲーム進行
+class Game
+    def start
+        puts "戦争を開始します"
+    end
+
+end
+
+
+# プレイヤー（２人）の作成
+class Player
+end
+
+game = Game.new()
+game.start
