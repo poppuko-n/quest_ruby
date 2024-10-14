4f6ac13eb2fc1ea2c3db602f9c34c76958fadd64
poppuko-n <souta0609@icloud.com>
2024-10-14 11:20:35 +0900

    カードをプレイヤーに配分

--- war.rb
+++ war.rb
@@ -34,19 +34,49 @@ class Deck
         @cards.map(&:name).join(", ")  # カードの名前をカンマ区切りで返す
     end
 
+    # カードを人数毎（今回は２人）に分ける
+    def deal
+        @cards.each_slice(@cards.size / 2).to_a
+    end
 end
 
 
 # ゲーム進行
 class Game
-    def prestart
-        puts "戦争を開始します"
+    def initialize
+        @deck = Deck.new
+        @players = create_players
+    end
+
+    def create_players
+        @deck.deal.map.with_index do |cards, index|
+            Player.new("プレイヤー#{index}", cards)
+        end
+    end
+
+    def players
+        return @players
     end
 end
 
 # プレイヤー（２人）の作成
 class Player
+    attr_reader :name, :hand
+
+    def initialize(name, hand)
+        @name = name
+        @hand = hand
+    end
+
+    def name
+        return @name
+    end
+
+    def hand
+        return @hand
+    end
 end
 
+game = Game.new
 deck = Deck.new
-puts deck.to_s
+puts game.players[1].hand.map(&:name) 
