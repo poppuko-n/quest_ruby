b911d5f0a821d87634c1a549f0c4711dd54d244f
poppuko-n <souta0609@icloud.com>
2024-10-14 20:22:01 +0900

    ステップ1動作確認完了

--- war.rb
+++ war.rb
@@ -1,7 +1,7 @@
 # ５２枚のカード、変数をもたせる
 class Card
     SUITS = ["スペード", "ハート", "ダイアモンド", "クラブ"]
-    RANKAS = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+    RANKS = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
     attr_reader :suit, :rank
 
     def initialize(suit, rank)
@@ -10,7 +10,11 @@ class Card
     end
 
     def name
-        return "#{@suit}の#{@rank}です"
+        return "#{@suit}の#{@rank}です。"
+    end
+
+    def value
+        RANKS.index(@rank)
     end
 end
 
@@ -19,7 +23,7 @@ class Deck
     def initialize
         @cards = []
         Card::SUITS.each do |suit|
-            Card::RANKAS.each do |rank|
+            Card::RANKS.each do |rank|
                 @cards << Card.new(suit, rank)
             end
         end
@@ -30,53 +34,65 @@ class Deck
         @cards.shuffle!
     end
 
-    def to_s
-        @cards.map(&:name).join(", ")  # カードの名前をカンマ区切りで返す
-    end
-
-    # カードを人数毎（今回は２人）に分ける
+    # カードを人数毎（今回は２人）に分けるが、まだ配らない
     def deal
         @cards.each_slice(@cards.size / 2).to_a
     end
 end
 
+# プレイヤー（２人）の作成し、分けたカードを手札として渡す
+class Player
+    attr_reader :name, :hand
+
+    def initialize(name, cards)
+        @name = name
+        @hand = cards
+    end
+
+    def play_card
+        hand.shift
+    end
+
+end
 
 # ゲーム進行
 class Game
+    # シャッフルしたデッキを準備し、各プレイヤーに手札を渡す
     def initialize
         @deck = Deck.new
         @players = create_players
     end
+    
+    # テーブルにカードを置く
+    def start
+        puts "戦争を開始します。"
+        puts "カードが配られました"
+        puts "戦争!"
+        to_table = @players.map(&:play_card)
+        to_table.each.with_index do |card, i|
+            puts "#{@players[i].name}のカードは#{card.name}"
+        end
+        buttle(to_table)
+    end
 
+    # 手札を持ったプレイヤーを作成
     def create_players
         @deck.deal.map.with_index do |cards, index|
-            Player.new("プレイヤー#{index}", cards)
+            Player.new("プレイヤー#{index+1}", cards)
         end
     end
 
-    def players
-        return @players
-    end
-end
-
-# プレイヤー（２人）の作成
-class Player
-    attr_reader :name, :hand
-
-    def initialize(name, hand)
-        @name = name
-        @hand = hand
-    end
-
-    def name
-        return @name
-    end
-
-    def hand
-        return @hand
+    #テーブルに置かれたカードを比較する
+    def buttle(to_table)
+        values = to_table.map(&:value)
+        winner_player = values.index(values.min)
+        if values.uniq.size == 1
+            puts "引き分けです。"
+        else
+            puts "#{@players[winner_player].name}が勝ちました。"
+        end
     end
 end
 
 game = Game.new
-deck = Deck.new
-puts game.players[1].hand.map(&:name) 
+game.start
